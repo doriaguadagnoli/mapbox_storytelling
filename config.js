d3d9050a192aa3d73f27db66aefbed343566ad00
doriaguadagnoli <doriaguadagnoli@gmail.com>
2025-05-08 18:49:08 -0400

    updated image

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ var config = {
             alignment: 'centered',
             hidden: false,
             title: 'The Color Purple',
-            image: './assets/color_purple_cover.jpg',
+            image: '/Users/doriaguadagnoli/Desktop/color_purple_cover.jpg',
             description: 'First, we will inspect the southern economic exchanges between the women in <em>The Color Purple<em>',
             location: {
                 center: [-90.3150946, 38.6605671],
