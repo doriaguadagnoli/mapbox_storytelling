fad4970bbc7bc7b096c7c113b9f4046eb7cbf2a5
doriaguadagnoli <doriaguadagnoli@gmail.com>
2025-05-08 15:55:51 -0400

    zoom changed

--- config.js
+++ config.js
@@ -59,7 +59,7 @@ var config = {
             description: 'Sofia being payed by the mayor',
             location: {
                 center: [-83.557033670263, 32.999402580951326], 
-                zoom: 3,
+                zoom: 8,
                 pitch: 60,
                 bearing: -43.2,
                 // flyTo additional controls-
